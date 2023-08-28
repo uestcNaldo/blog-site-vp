381c36ddf8e70ea4a7a78576091a3274b30e0a88
Naldo Wang <1165648703@qq.com>
2023-08-28 16:53:39 +0800

    添加README和LICENSE

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig, defineConfigWithTheme } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "VitePress Site",
-  description: "A VitePress Site",
+  srcDir: './src',
+  lang: 'zh-Hans',
+  title: "VP站点标题",
+  titleTemplate: 'VP站点开发',
+  description: "site description",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: '/logo.svg',
+
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' }
---
+++ README.md
@@ -0,0 +1,17 @@
+# 静态站点开发(VitePress)
+
+使用 **[VitePress](https://vitepress.dev/)** 搭建的站点
+
+## 使用站点
+
+站点配置文件在 `(root)/.vitepress/config.ts`
+
+### 开发
+```bash
+npm run site:dev
+```
+
+### 构建
+```bash
+npm run site:build
+```
